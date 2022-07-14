d484b6ed8e4df7a53e65f7893a2e263da85b37b2
Elisabeth Lenkat <Elisabeth.Lenkat@promosconsult.de>
2022-07-14 10:49:43 +0200

    Initial Commit

---
+++ LoC_Counter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LoC_Counter
+//
+//  Created by Elisabeth Lenkat on 14.07.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LoC_Counter/LoC_CounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoC_CounterApp.swift
+//  LoC_Counter
+//
+//  Created by Elisabeth Lenkat on 14.07.22.
+//
+
+import SwiftUI
+
+@main
+struct LoC_CounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
